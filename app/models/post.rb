0dfd2b1ea06c9235c0495924c8058feb69179ea4
Dmitry <dima.pavlovich.99@gmail.com>
2019-03-16 18:12:39 +0300

    Revert "add summary to posts"
    good

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,6 @@
 class Post < ApplicationRecord
 
   validates :title, presence: true
-<<<<<<< HEAD
-  validates :body, length: {minimum: 20}
-  
-=======
   validates :body, length: {minimum: 200}
 
->>>>>>> posts
 end
